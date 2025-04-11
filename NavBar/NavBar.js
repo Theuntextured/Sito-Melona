b25ffbf93299e36ad534fea0a3fb0be35787dc7f
Theuntextured <theuntextured@hotmail.com>
2025-04-11 02:55:03 +0200

    Some navbar progress

---
+++ NavBar/NavBar.js
@@ -0,0 +1,25 @@
+const navbar = document.querySelector('nav');
+const scrollThreshold = 100;
+const content = document.getElementById("Content");
+
+function OnResize(){
+    content.style.paddingTop = `${navbar.offsetHeight}px`;    
+}
+
+OnResize();
+
+window.addEventListener('resize', OnResize);
+
+window.addEventListener('scroll', function () {
+    if (window.scrollY > scrollThreshold) {
+        if (!navbar.classList.contains('fixed')) {
+            navbar.classList.add('fixed');
+            navbar.classList.remove('static');  // Remove static when fixed
+        }
+    } else {
+        if (!navbar.classList.contains('static')) {
+            navbar.classList.add('static');
+            navbar.classList.remove('fixed');  // Remove fixed when static
+        }
+    }
+});

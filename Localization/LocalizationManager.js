dceab2c24b6fd8108c2fb0e9349281ed5ff41b65
Theuntextured <theuntextured@hotmail.com>
2025-04-24 18:25:48 +0200

    Localization support

---
+++ Localization/LocalizationManager.js
@@ -0,0 +1,74 @@
+let Language = "it";
+let LocalizationData;
+function GetLanguage() {
+    Language = document.cookie
+        .split('; ')
+        .find(row => row.startsWith('language='))
+        ?.split('=')[1];
+    if(!Language)
+        Language = navigator.language;
+    if(!Language){
+        Language = "en";
+    }
+    Language = Language.split("-")[0];
+    return Language;
+}
+
+function SetLanguage(LangCode) {
+    if(Language === LangCode)
+        return;
+    document.cookie = `language=${LangCode}; path=/; max-age=31536000`; // 1 year
+    Language = LangCode;
+    location.reload();
+}
+
+function fetchJSONData() {
+    fetch(new URL("./LocalizationTable.json", import.meta.url))
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(data => ApplyLocalization(data))
+        .catch(error => console.error('Failed to fetch data:', error));
+}
+fetchJSONData();
+
+function GetTextFromKey(key){
+    let RelevantData = LocalizationData[key];
+    if(!RelevantData){
+        console.log("Warning: Invalid localization key provided: '" + key + "'");
+        return "[" + key + "]";
+    }
+    let FoundText = RelevantData[Language];
+    if(!FoundText){
+        FoundText = RelevantData["en"];
+    }
+    if(!FoundText){
+        FoundText = RelevantData["it"];
+    }
+    if(!FoundText){
+        return "[" + key + "]";
+    }
+    return FoundText;
+}
+
+function ApplyLocalization(Data = null){
+    GetLanguage();
+    if(Data != null)
+        LocalizationData = Data;
+    const ToLocalize = document.getElementsByClassName("localized");
+    
+    for( let i = 0; i < ToLocalize.length; i++){
+        const Current = ToLocalize[i];
+        const key = Current.getAttribute("content-key");
+        Current.innerHTML = GetTextFromKey(key);
+        //let TextNode = document.createTextNode(GetTextFromKey(key)); 
+        //Current.appendChild(TextNode);
+    }
+
+    const LanguageSelector = document.getElementById("LanguageSelector");
+    LanguageSelector.Value = Language;
+    LanguageSelector.addEventListener("change", (e) => {SetLanguage(e.target.value);});
+}
